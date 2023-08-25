18d1926616e764383bcb18e38a9460900ce3f15d
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-08-25 19:37:18 +0530

    Updates

---
+++ precache-manifest.9344aa2e471d132a23358d1e793db590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f22740ec80c40856d8d",
+    "url": "/react-tut/static/css/main.da7b0d26.chunk.css"
+  },
+  {
+    "revision": "3f22740ec80c40856d8d",
+    "url": "/react-tut/static/js/main.3f22740e.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/react-tut/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "868c5abacb8dad9fc291",
+    "url": "/react-tut/static/js/2.868c5aba.chunk.js"
+  },
+  {
+    "revision": "f8c4afca418277635ff8",
+    "url": "/react-tut/static/js/runtime~main.f8c4afca.js"
+  },
+  {
+    "revision": "51aff6296bfea2ec8b29760f4600dffb",
+    "url": "/react-tut/index.html"
+  }
+];
